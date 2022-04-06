0e4c746ca1d3791341abb77c9f7b9959cf5dee4c
Nicolás Pargade <nicojpargade@gmail.com>
2022-04-06 14:49:44 -0300

    agregamos algunos archivos más y corregí error de babel "npm install --savenpm install --save @babel/polyfill"

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: "./src/index.js",
+    entry: ['@babel/polyfill', './src/index.js'],
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "main.js",
@@ -24,12 +24,12 @@ module.exports = {
         ],
     },
     plugins: [
-        new HtmlWebPackPlugin([
+        new HtmlWebPackPlugin(
             {
                 inject: true,
                 template: "./public/index.html",
                 filename: "./index.html",
             },
-        ]),
+        ),
     ],
 };
